f853e48738c3ce8f083510be21266c2fd458a6e6
itsme12453 <heetvirola110508@gmail.com>
2022-03-22 12:08:07 +0000

    update

--- pages/anime/[slug].js
+++ pages/anime/[slug].js
@@ -8,33 +8,38 @@ import WeeklyTopCard from "/components/WeeklyTopCard"
 import AnimeSelection from "/components/AnimeSelection"
 import RandomBackground from "/components/RandomBackground"
 
-function MyApp(){
+function MyApp({ data }){
+    const episodes = JSON.parse(data);
     const { asPath } = useRouter();
-    const anime_id = decodeURI(asPath.split("/")[2]).replace(/-/g, " ")
+    const anime_id = decodeURI(asPath.split("/")[2]).replace(/-/g, " ");
+    const url = decodeURIComponent(episodes[0]["src"].replace("/stream?url=", ""));
+
+    console.log(url);
 
     return (
         <div width="100%" height="100%">
             <head>
-                <script src="https://cdn.plyr.io/3.6.12/plyr.js"/>
+                <script src="https://cdn.plyr.io/3.6.12/plyr.js"></script>
                 <link rel="stylesheet" href="https://cdn.plyr.io/3.6.12/plyr.css" />
             </head>
 
-            <NavBar/>
-
-            <Box postion="fixed" style={{zIndex: "-1"}}>
-                <Image src="https://i.pinimg.com/originals/32/4a/51/324a511fb2d414841cf6458088aa5d35.jpg" width="100%" position="relative"/>
-                <Box top="0px" width="100%" height="100%" bgGradient="linear(to-b, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 50%, rgba(0, 0, 0, 1) 55%)" position="absolute" />
-            </Box>
-
-            <Box width="600px" top="150px" left="0" right="0" margin="auto" position="absolute" style={{ zIndex: "1" }}>
-                <iframe width="600px" height="400px" src="https://aniflake-proxy-server.herokuapp.com/player?anime_id=death-note&eps_number=37" allowfullscreen/>
-            </Box>
+            <video id="player" playsinline controls>
+                <source src={url} type="video/mp4" />
+            </video>
 
             <Script>
-                {/* var player = new Plyr("#player"); */}
+                var player = new Plyr("#player");
             </Script>
         </div>
     )
 }
+
+export async function getServerSideProps(context){
+    const res = await fetch(`https://aniflake-proxy-server.herokuapp.com/get_streaming_sources?anime_id=death-note&eps_number=1`)
+    const newData = await res.text()
+    const data = [newData]
+
+    return { props: {data} }
+  }
 
 export default MyApp
