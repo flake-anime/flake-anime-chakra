66f9d89999b4f255cad0bef9a7fb3e06df63c080
dunkshotenthusiast <18sgupta@heckgrammar.co.uk>
2022-05-09 17:46:45 +0100

    made the navbar invisible under 576 width px

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import '../styles/globals.css'
 import '../styles/loading.css'
+import "../styles/discord.css"
 
 const theme = extendTheme({
     colors: {
